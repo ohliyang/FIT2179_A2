74c674afd767645b5ca38a2abd8c23ac1db285b7
ohliyang <lohh0002@student.monash.edu>
2026-05-20 21:43:36 +1000

    A2 all files

---
+++ vega_lite_vis.js
@@ -0,0 +1,44 @@
+var vg_1 = "chart1.json";
+vegaEmbed("#map_chart", vg_1).then(function (result) { }).catch(console.error);
+
+var vg_2 = "chart2.json";
+vegaEmbed("#comet_chart", vg_2).then(function (result) { }).catch(console.error);
+
+var vg_3 = "chart3.json";
+vegaEmbed("#line_chart", vg_3).then(function (result) { }).catch(console.error);
+
+var vg_4 = "chart4.json";
+vegaEmbed("#isotype", vg_4).then(function (result) { }).catch(console.error);
+
+var vg_5 = "chart5.json";
+vegaEmbed("#diverging_bar", vg_5).then(function (result) { }).catch(console.error);
+
+var vg_6 = "chart6.json";
+vegaEmbed("#heat_map", vg_6).then(function (result) { }).catch(console.error);
+
+var vg_7 = "chart7.json";
+vegaEmbed("#scatterplot", vg_7).then(function (result) { }).catch(console.error);
+
+var vg_8 = "chart8.json";
+vegaEmbed("#pie_chart", vg_8).then(function (result) { }).catch(console.error);
+
+var vg_9 = "chart9.json";
+vegaEmbed("#dumbbell_chart", vg_9).then(function (result) { }).catch(console.error);
+
+var vg_10 = "chart10.json";
+vegaEmbed("#slope_chart", vg_10).then(function (result) { }).catch(console.error);
+
+var vg_11 = "chart11.json";
+vegaEmbed("#sankey_diagram", vg_11).then(function (result) { }).catch(console.error);
+
+var vg_12 = "chart12.json";
+vegaEmbed("#tree_map", vg_12).then(function (result) { }).catch(console.error);
+
+var vg_13 = "chart13.json";
+vegaEmbed("#waffle_chart", vg_13).then(function (result) { }).catch(console.error);
+
+var vg_14 = "chart14.json";
+vegaEmbed("#grouped_bar", vg_14).then(function (result) { }).catch(console.error);
+
+var vg_15 = "chart15.json";
+vegaEmbed("#stacked_bar", vg_15).then(function (result) { }).catch(console.error);
